e9e6ff8c281577819f51ee092bf17b3f260a9710
Unni Krishnan B V <unnik9218@gmail.com>
2025-07-25 00:14:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import Input_bar from './components/Input_bar'
-import './App.css'
-import ButtonEg from './components/ButtonEg'
+import { useState } from 'react';
+import Input_bar from './components/Input_bar';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Submit from './components/Submit';
 import Sidebar from './components/Sidebar';
@@ -9,30 +8,29 @@ import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Details from './components/Details';
 import About from './components/About';
-import Scrollbar from './components/Scrollbar'
 import Settings from './components/Settings';
-import { AuthProvider } from './AuthContext';
-import Iconeg from "./components/Iconeg";
 import PrintingPreview from './components/PrintingPreview';
-function App() {
+import { AuthProvider } from './AuthContext';
 
+function App() {
   return (
-      <AuthProvider>
+    <AuthProvider>
       <BrowserRouter>
-      <Sidebar/>
-        {/* <Scrollbar/> */}
+        <Sidebar />
         <Routes>
-          <Route path='/' element={<Input_bar/>}/>
-          <Route path='/submit' element={<Submit/>}/>
-          <Route path='/signup' element={<Signup/>} />
-          <Route path="/signin" element={<Signin/>} />
-          <Route path="/details" element={<Details/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/settings" element={<Settings/>}  />
-          <Route path="/printingpreview" element={<PrintingPreview/>} />
+          <Route path="/" element={<Input_bar />} />
+          <Route path="/submit" element={<Submit />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/printingpreview" element={<PrintingPreview />} />
+          {/* Fallback route for 404 */}
+          <Route path="*" element={<h2 style={{ textAlign: 'center', padding: '2rem' }}>404 - Page Not Found</h2>} />
         </Routes>
       </BrowserRouter>
-      </AuthProvider>
+    </AuthProvider>
   );
 }
 
