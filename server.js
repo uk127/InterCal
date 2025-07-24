0b52245cc771628f7d81282c1dd9c18a116b7d1a
Unni Krishnan B V <unnik9218@gmail.com>
2025-07-24 23:32:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,42 +6,52 @@ import subjectRoutes from "./routes/subject.js";
 import externalRoutes from "./routes/external.js";
 import settingsRoutes from "./routes/settings.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
-// var x, y, total;
-// var catMarks;
-// var assignmentMarks;
-
-
+const PORT = process.env.PORT || 10000;
 const url = process.env.MONGO_URL;
 const dbName = 'project_database';
 let db;
 
 async function startServer() {
-    try {
-        const client = await MongoClient.connect(url);
-        console.log("connected to mongodb");
-        db = client.db(dbName);
+  try {
+    const client = await MongoClient.connect(url);
+    console.log("connected to mongodb");
+    db = client.db(dbName);
 
-        app.use((req, res, next) => {
-            req.db = db;
-            next();
-        });
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
+
+    // Routes
     app.use("/auth", authRoutes);
     app.use("/subject", subjectRoutes);
-    app.use("/exter",externalRoutes);
-    app.use("/settings",settingsRoutes);
+    app.use("/exter", externalRoutes);
+    app.use("/settings", settingsRoutes);
+
+    // Serve static React files
+    app.use(express.static(path.join(__dirname, 'dist')));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    });
 
     app.listen(PORT, () => {
-        console.log(`server is running at ${'http://localhost:5000/'}`)
-    })
-    }catch(err){
-        console.error("failed to connect to mongodb");
-    }
+      console.log(`server is running at http://localhost:${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("failed to connect to mongodb", err);
+  }
 }
+
 startServer();
